a01ca9619e848dce52d0fcbbb6eae86ecf67bac7
Александр Батанов <pokertrend@yandex.ru>
2024-02-16 20:48:42 +0400

    added allowed_hosts

--- currency_rates/currency_rates_project/settings.py
+++ currency_rates/currency_rates_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '0.0.0.0',
+    'localhost',
+]
 
 
 # Application definition
